119c3cd7be501c2958070b51c1b2336f5884f0f5
IYarmak <94400040+IYarmak@users.noreply.github.com>
2024-02-07 13:00:43 +0300

    appYarmk

---
+++ appYarmak.js
@@ -0,0 +1,44 @@
+AFRAME.registerComponent("mytarget", {
+    init: function () {
+        const webButton = document.querySelector("#web-button");
+        const phoneButton = document.querySelector("#phone-button");
+        const messageButton = document.querySelector("#message-button");
+        const telegramButton = document.querySelector("#telegram-button");
+        const vkButton = document.querySelector("#vk-button");
+        const instButton = document.querySelector("#inst-button");
+        const video = document.querySelector("#video-TMRS");
+        const youtubeButton = document.querySelector("#vid");
+        this.el.addEventListener("targetFound", (event) => {
+            console.log("target found");
+            video.play();
+
+            webButton.addEventListener("click", (event) => {
+                window.location.href = "https://tomorrowsrender.com/";
+            });
+            phoneButton.addEventListener("click", (event) => {
+                window.open("tel:+79111230763");
+            });
+            messageButton.addEventListener("click", (event) => {
+                window.location.href = "https://t.me/iyarmak";
+            });
+            telegramButton.addEventListener("click", (event) => {
+                window.location.href = "https://t.me/tomorrowsrender";
+            });
+            vkButton.addEventListener("click", (event) => {
+                window.location.href = "https://vk.com/tomorrowsrender";
+            });
+            instButton.addEventListener("click", (event) => {
+                window.location.href = "https://instagram.com/i.yarmak";
+            });
+            youtubeButton.addEventListener("click", (event) => {
+                window.location.href = "https://www.youtube.com/@tomorrowsrender";
+            });
+        });
+
+        this.el.addEventListener("targetLost", (event) => {
+            console.log("target found");
+            video.pause();
+        });
+        //this.el.emit('targetFound');
+    },
+});
